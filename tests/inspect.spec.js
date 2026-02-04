1ab47aa7c3f27dc80f6d579de234dd850e1b4fd5
nandha.rajendran <nandha.rajendran@videoslots.com>
2026-02-04 01:24:42 +0100

    removed a local file

--- tests/inspect.spec.js
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-
-// Simple inspect test to open the AUT and pause so you can interactively inspect selectors
-test( 'inspect page for selectors', async({ page }) => {
-    await page.goto( 'https://testapp.fairlo.se/application' );
-    // Pause to allow headed interactive inspection
-    await page.pause();
-});
